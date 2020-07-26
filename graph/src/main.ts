4be933240736cc2667b02eefeea3d24f86ee8af2
shioda999 <k0shig0e17@gmail.com>
2020-07-26 21:03:33 +0900

    逆三角関数に対応

--- graph/src/main.ts
+++ graph/src/main.ts
@@ -36,25 +36,27 @@ function getpoint(formula: string[], left: number, right: number, d: number) {
         if (isNaN(y)) continue
         px.push(x)
         py.push(y)
-        if (isNaN(max_y)) max_y = min_y = y, console.log(y)
+        if (isNaN(max_y)) max_y = min_y = y
         else max_y = Math.max(max_y, y), min_y = Math.min(min_y, y)
     }
     if (isNaN(max_y)) max_y = min_y = 0
     max_y = Math.min(10, max_y + d)
     min_y = Math.max(-10, min_y - d)
-    //alert(max_y + " " + min_y)
     return {"x": px, "y": py, "max_y": max_y, "min_y": min_y, "max_x": right, "min_x": left, "v_num": px.length}
 }
 function conv(str: string) {
     str = str.replace(/\*/g, "\\times ")
     str = str.replace(/\//g, "\\div ")
-    str = str.replace(/\\?sin/g, "\\sin ")
-    str = str.replace(/\\?cos/g, "\\cos ")
-    str = str.replace(/\\?tan/g, "\\tan ")
-    str = str.replace(/\\?log/g, "\\log ")
-    str = str.replace(/\\?log 2/g, "\\log_2 ")
-    str = str.replace(/\\?log 10/g, "\\log_{10} ")
-    str = str.replace(/\\?pi/g, "\\pi ")
+    str = str.replace(/sin/g, "\\sin ")
+    str = str.replace(/cos/g, "\\cos ")
+    str = str.replace(/tan/g, "\\tan ")
+    str = str.replace(/arc\\sin/g, "\\arcsin ")
+    str = str.replace(/arc\\cos/g, "\\arccos ")
+    str = str.replace(/arc\\tan/g, "\\arctan ")
+    str = str.replace(/log/g, "\\log ")
+    str = str.replace(/log 2/g, "\\log_2 ")
+    str = str.replace(/log 10/g, "\\log_{10} ")
+    str = str.replace(/pi/g, "\\pi ")
     for (let i = 0, i2; i < str.length - 1; i++){
         if (str[i] === "^" && str[i + 1] === "(") {
             let nest = 0
@@ -67,7 +69,6 @@ function conv(str: string) {
             }
             if (i2 < str.length) str = str.slice(0, i + 1) + "{" + str.slice(i + 2, i2) + "}" + str.slice(i2 + 1)
             else str = str.slice(0, i + 1) + "{" + str.slice(i + 2) + "}"
-            i = i2
         }
         else if (str[i] === "^" && str[i + 1] !== "(") {
             i2 = i + 1
@@ -76,7 +77,6 @@ function conv(str: string) {
             for (; i2 < str.length; i2++)if (!('0' <= str[i2] && str[i2] <= '9')) break
             if (i2 < str.length) str = str.slice(0, i + 1) + "{" + str.slice(i + 1, i2) + "}" + str.slice(i2)
             else str = str.slice(0, i + 1) + "{" + str.slice(i + 1) + "}"
-            i = i2
         }
     }
     return str
@@ -86,5 +86,11 @@ function preparation(text: string): string {
     text = text.replace(/}/g, ")")
     text = text.replace(/\[/g, "(")
     text = text.replace(/\]/g, ")")
+    text = text.replace(/sin\^-1/g, "arcsin")
+    text = text.replace(/asin/g, "arcsin")
+    text = text.replace(/cos\^-1/g, "arccos")
+    text = text.replace(/acos/g, "arccos")
+    text = text.replace(/tan\^-1/g, "arctan")
+    text = text.replace(/atan/g, "arctan")
     return text
 }
