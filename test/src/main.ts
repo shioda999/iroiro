61887d7cdba4e3890960afc3ea9cc790fdcba7e1
shioda999 <k0shig0e17@gmail.com>
2021-08-23 10:42:24 +0900

    commit

--- test/src/main.ts
+++
@@ -1,6 +0,0 @@
-import {Screen} from './Screen'
-import {SceneManager} from './sceneManager'
-
-const screen = Screen.init()
-const container = screen.getContainer()
-const scene = SceneManager.init(container)
