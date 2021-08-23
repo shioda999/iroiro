de233a0fddf970316fa2571c1510c5803fefd096
shioda999 <k0shig0e17@gmail.com>
2021-08-23 12:39:37 +0900

    commit

--- katex_test/src/main.ts
+++ katex_test/src/main.ts
@@ -10,7 +10,7 @@ const range = menu.children["move"]
 const auto_update: any = menu.children["auto_update"]
 const font_size = menu.children["font_size"]
 
-setTimeout(() => setup(), 200)
+setTimeout(() => setup(), 1000)
 
 function setup() {
     set_button_option()
